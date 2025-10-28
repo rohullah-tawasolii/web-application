4a2c00b684892d323f033defd7c71d85ac02bed5
tawasoli <rohullah.tawasoli@outlook.com>
2025-10-28 12:39:55 +0330

    update home page and add new pic and delete old pic + add middleware to project

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const requireAuth = (req, res, next) => {
+    const secretKey = 'your-secret-key';
+    const {token} = req.cookies
+
+    if (token){
+        try {
+            const decoded = jwt.verify(token, secretKey);
+            if (decoded){
+                res.locals.decoded = decoded;
+                next();
+            } else {
+                res.redirect("/auth/login")
+            }
+        } catch (error){
+            res.redirect("/auth/login")
+        }
+    } else {
+        res.redirect("/auth/login")
+    }
+}
+
+module.exports = {
+    requireAuth
+}
--- routes/home.route.js
+++ routes/home.route.js
@@ -3,9 +3,18 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   const posts = [
-    { title: 'First Post', image: '/images/1.jpg', content: 'This is the first post.' },
-    { title: 'Second Post', image: '/images/2.jpg', content: 'This is the second post.' },
-    { title: 'Third Post', image: '/images/3.jpg', content: 'This is the third post.' }
+    { title: 'Modern Daily Podcast', image: '/images/img (1).jpg', content: 'This is the first post.' },
+    { title: 'Top 10 - Shocking moments', image: '/images/img (2).jpg', content: 'This is the second post.' },
+    { title: 'INDIA', image: '/images/img (3).jpg', content: 'This is the third post.' },
+    { title: 'Mindset', image: '/images/img (4).jpg', content: 'This is the first post.' },
+    { title: 'Live Streaming - Game', image: '/images/img (5).jpg', content: 'This is the second post.' },
+    { title: 'Travel Vlog', image: '/images/img (6).jpg', content: 'This is the first post.' },
+    { title: 'Daily Vlog', image: '/images/img (7).jpg', content: 'This is the second post.' },
+    { title: 'Journey to the Heart', image: '/images/img (8).jpg', content: 'This is the first post.' },
+    { title: 'Holi Sweets', image: '/images/img (9).jpg', content: 'This is the second post.' },
+    { title: '1M Views', image: '/images/img (10).jpg', content: 'This is the first post.' },
+    { title: 'PROFITABLE TRADING STRATEGY', image: '/images/img (11).jpg', content: 'This is the second post.' },
+    { title: 'Top 10 - Shocking moments', image: '/images/img (12).jpg', content: 'This is the first post.' }
   ];
 
   res.render('home', { title: 'Shahrakesang.com', posts });
